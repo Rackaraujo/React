c1dbac7992e217be6cbb55a90b92fe8acdf64660
Rackaraujo <raqara77@gmail.com>
2024-08-01 11:10:32 -0300

    Primeira versão do Etecflix

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <>
+      <Header/>
+      <h1>ETEC MCM</h1>
+      <p>Olá mundo!</p>
+    </>
+  );
+}
+
+export default App;
